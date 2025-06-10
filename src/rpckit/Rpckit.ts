3125d4fd72ee88defd7255dbc6def4888ba754ce
hongguo <hongguo@qunhemail.com>
2025-06-11 01:33:31 +0800

    扩展Rpckit类

--- src/rpckit/Rpckit.ts
+++ src/rpckit/Rpckit.ts
@@ -1,5 +1,5 @@
 import { asyncThrow } from '../common/common';
-import { ServTerminal, ServTerminalConfig } from '../terminal/ServTerminal';
+import { EServTerminal, ServTerminal, ServTerminalConfig } from '../terminal/ServTerminal';
 import { ServGlobalServiceConfig, ServGlobalServiceManager } from './ServGlobalServiceManager';
 import { EventEmitter } from 'eventemitter3';
 
@@ -18,19 +18,34 @@ export interface RpckitConfig {
      */
     service?: ServGlobalServiceConfig;
 }
+
 export class Rpckit extends EventEmitter {
+    private static instance: Rpckit | null = null;
+    
     namespace: string;
-
     service: ServGlobalServiceManager;
-
     protected terminals: ServTerminal[];
 
-    constructor(namespace?: string) {
+    private constructor(namespace?: string) {
         super();
-
         this.namespace = namespace || '';
     }
 
+    /**
+     * 获取Rpckit单例实例
+     */
+    public static getInstance(namespace?: string): Rpckit {
+        if (!Rpckit.instance) {
+            Rpckit.instance = new Rpckit(namespace);
+            Rpckit.instance.init();
+        }
+        return Rpckit.instance;
+    }
+
+    /**
+     * 初始化
+     * @param config 初始化配置 
+     */
     init(config?: RpckitConfig) {
         config = config || {};
         
@@ -40,6 +55,9 @@ export class Rpckit extends EventEmitter {
         this.service.init(config && config.service);
     }
 
+    /**
+     * 释放资源
+     */
     release() {
         const terminals = this.terminals;
         this.terminals = [];
@@ -49,43 +67,69 @@ export class Rpckit extends EventEmitter {
         });
 
         this.service.release();
+        Rpckit.instance = null;
     }
 
+    /**
+     * 创建终端
+     * @param config 
+     * @returns 
+     */
     createTerminal(config: ServTerminalConfig) {
+        if (!config || !config.id || !config.type) {
+            throw new Error('[RPCKIT] Invalid terminal configuration: id and type are required.');
+        }
+
         if (this.terminals.some((item) => {
             return item.id === config.id && item.type === config.type;
         })) {
-            throw new Error(`[SERVKIT] Terminal [${config.id}:${config.type}] conflicts.`);
+            throw new Error(`[RPCKIT] Terminal [${config.id}:${config.type}] already exists.`);
         }
 
         const terminal = new ServTerminal(this);
         terminal.init(config);
-
+        this.terminals.push(terminal);
         return terminal;
     }
 
     destroyTerminal(terminal: ServTerminal) {
+        if (!terminal) {
+            throw new Error('[RPCKIT] Cannot destroy undefined terminal.');
+        }
         terminal.release();
     }
 
-    onTerminalInit(terminal: ServTerminal) {
-        this.terminals.push(terminal);
-    }
-
     onTerminalRelease(terminal: ServTerminal) {
+        if (!terminal) {
+            throw new Error('[RPCKIT] Cannot release undefined terminal.');
+        }
         const i = this.terminals.indexOf(terminal);
         if (i >= 0) {
             this.terminals.splice(i, 1);
         }
     }
+
+    /**
+     * 获取所有终端
+     */
+    getTerminals(): ServTerminal[] {
+        return [...this.terminals];
+    }
+
+    /**
+     * 检查终端是否存在
+     */
+    hasTerminal(id: string, type: EServTerminal): boolean {
+        return this.terminals.some(terminal => terminal.id === id && terminal.type === type);
+    }
+
+    /**
+     * 获取指定终端
+     */
+    getTerminal(id: string, type: EServTerminal): ServTerminal | undefined {
+        return this.terminals.find(terminal => terminal.id === id && terminal.type === type);
+    }
 }
 
-let sInstance: Rpckit = undefined!;
-try {
-    sInstance = new Rpckit();
-    sInstance.init();
-} catch (e) {
-    asyncThrow(e);
-}
-
-export const rpckit = sInstance;
+// 导出单例实例
+export const rpckit = Rpckit.getInstance();
